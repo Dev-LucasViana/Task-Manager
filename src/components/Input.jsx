907217ab0eede300530efc7b5ba6285b5c9b97bf
Lucas Viana <nilureis@gmail.com>
2025-05-02 21:24:54 -0300

    outra correção

--- src/components/Input.jsx
+++
@@ -1,9 +0,0 @@
-// function Input(props) {
-//   return (
-//     <input
-//       className="border border-slate-300 outline-slate-400 px-4 py-2 rounded"
-//       {...props}
-//     />
-//   );
-// }
-// export default Input;
